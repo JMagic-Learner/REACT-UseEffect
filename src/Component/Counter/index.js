2ef40577185e7aa2da38817e36fb7c7d422b8ec2
JasonDesktop <MaJason93@gmail.com>
2022-06-17 09:59:56 -0700

    Testing more useEffect

--- src/Component/Counter/index.js
+++ src/Component/Counter/index.js
@@ -5,7 +5,9 @@ import {useEffect, useState} from 'react'
 const Counter = () => {
     const [count, setcount] = useState(0)
     const [isAlert, setIsAlert] = React.useState(false)
+    const [alertUIDormant, setAlertUIActive] = useState("You have not clicked yet")
     const counterRef = React.useRef(count)
+    const alertUIRef = React.useRef(alertUIDormant)
     const addition = () => {
         setcount(count+1)
     }
@@ -21,6 +23,16 @@ const Counter = () => {
         }, 5000)
     }
 
+    const alertUI = () => {
+        if (count == counterRef) {
+            return <p> You have not clicked yet</p>
+        } else if (count <=10 ) {
+            return <p> You have begun clicking the button</p>
+        } else {
+            return <h1> You have clicked many times </h1>
+        }
+    }
+
     useEffect(() => {
         if (isAlert) {
             alert(count)
@@ -33,12 +45,22 @@ const Counter = () => {
             console.log("This is the counterRef", counterRef)
             console.log("CounterRef should display the first reference or instance that count has been invoked.")
             console.log("This is the Count", count)
-            
+        }
+    }, [count])
+
+    useEffect(() => {
+        if (counterRef) {
+            setAlertUIActive('You have begun clicking the button')
+        }
+        if (alertUIRef) {
+            console.log(alertUIRef)
         }
     }, [count])
 
 
+
     return (
+
         <div id="counter-container">
             <h1> You have clicked {count} times</h1>
             <button onClick={addition}>
@@ -47,7 +69,11 @@ const Counter = () => {
             <button onClick={subtraction}>
                 Click to add
             </button>
+            <div id="alert-container">
+               <p id="alert-ui"> {alertUIDormant} </p>
+            </div>
         </div>
+        
     )
 }
 
